59e9c6d4982c501a85eecf40bb810d3a55dcdc06
vish7466 <abhi.vish7466@gmail.com>
2025-01-03 20:57:35 +0530

    Industry Standard written codes

--- src/in/iicons/main/TestApp.java
+++ src/in/iicons/main/TestApp.java
@@ -1,39 +1,45 @@
 package in.iicons.main;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import in.iicons.utility.JdbcUtil;
 
 public class TestApp {
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		// load and register the driver
-		Class.forName("com.mysql.cj.jdbc.Driver");
+	public static void main(String[] args) {
+		//Resources used
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
 		
-		// Establish the Connection
-		String url = "jdbc:mysql://localhost:3306/studentsrelationshipmanager";
-		String user = "root";
-		String password = "root1234";
-		Connection connection = DriverManager.getConnection(url, user, password);
-		
-		// Create a Statement object
-		Statement statement = connection.createStatement();
-		
-		// Execute the query
-		String sqlSelectQuery = "select sid, name, age, address from student";
-		ResultSet resultSet = statement.executeQuery(sqlSelectQuery);
-		System.out.println("SID\tNAME\tAGE\tADDRESS");
-		
-		// Process the resultSet
-		while (resultSet.next()) {
-			System.out.println(resultSet.getInt(1) + "\t" + 
-					resultSet.getString(2) + "\t" + resultSet.getInt(3) + "\t" + resultSet.getString(4));
-		}
-		
-		//close the resources
-		resultSet.close();
-		statement.close();
-		connection.close();
+		try {
+			//Getting the connection
+			connection = JdbcUtil.getDbConnection();
+			if (connection != null)
+				statement = connection.createStatement();
+			if (statement != null)
+				resultSet = statement.executeQuery("select sid, name, age, address from student");
+			if (resultSet != null) {
+				System.out.println("SID\tNAME\tAGE\tADDRESS");
+				while (resultSet.next()) {
+					System.out.println(resultSet.getInt(1) + "\t" + resultSet.getString(2) + "\t" + resultSet.getInt(3)+ "\t" + resultSet.getString(4));
+					}
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				//Closing the resources
+				try {
+					JdbcUtil.closeResources(resultSet, statement, connection);
+				} catch (SQLException e) {
+					e.printStackTrace();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
 	}
 }
+
--- src/in/iicons/utility/JdbcUtil.java
+++ src/in/iicons/utility/JdbcUtil.java
@@ -1,5 +1,36 @@
 package in.iicons.utility;
 
-public class JdbcUtil {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class JdbcUtil {
+	static {
+		// load and register the driver
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Connection getDbConnection() throws SQLException {
+		// Establish the Connection
+		String url = "jdbc:mysql://localhost:3306/studentsrelationshipmanager";
+		String user = "root";
+		String password = "root1234";
+		return DriverManager.getConnection(url, user, password);		
+	}
+	
+	public static void closeResources(ResultSet resultSet, Statement statement, Connection connection) throws SQLException {
+		if (resultSet != null)
+			resultSet.close();
+		if (statement != null)
+			statement.close();
+		if (connection != null)
+			connection.close();
+	}
 }
+
